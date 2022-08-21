7611713df631ca9606151bd0d27b9c593ae9da24
Dipesh-Patil <91935990+dipeshpatil123@users.noreply.github.com>
2022-08-21 19:31:51 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -10,15 +10,16 @@ from bot import AUTO_DELETE_MESSAGE_DURATION, LOGGER, status_reply_dict, status_
 from bot.helper.ext_utils.bot_utils import get_readable_message, setInterval
 
 
-def sendMessage(text: str, bot, message: Message):
+def sendMarkup(text: str, bot, message: Message, reply_markup: InlineKeyboardMarkup):
     try:
         return bot.sendMessage(message.chat_id,
                             reply_to_message_id=message.message_id,
-                            text=text, allow_sending_without_reply=True, parse_mode='HTML', disable_web_page_preview=True)
+                            text=text, reply_markup=reply_markup, allow_sending_without_reply=True,
+                            parse_mode='HTML', disable_web_page_preview=True)
     except RetryAfter as r:
         LOGGER.warning(str(r))
         sleep(r.retry_after * 1.5)
-        return sendMessage(text, bot, message)
+        return sendMarkup(text, bot, message, reply_markup)
     except Exception as e:
         LOGGER.error(str(e))
         return
