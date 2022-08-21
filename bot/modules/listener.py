a1b5867291983a386f7010fb9d647a709bf6251b
Dipesh-Patil <91935990+dipeshpatil123@users.noreply.github.com>
2022-08-21 21:25:47 +0530

    Update listener.py

--- bot/modules/listener.py
+++ bot/modules/listener.py
@@ -303,7 +303,11 @@ class MirrorLeechListener:
         b_uname = bot_d.username  
         botstart = f"http://t.me/{b_uname}"  
         buttons.buildbutton("View links in PM", f"{botstart}")
-        sendMarkup(msg + botpm, self.bot, self.message, buttons.build_menu(2))
+       # sendMarkup(msg + botpm, self.bot, self.message, buttons.build_menu(2))
+        reply_to = self.message.reply_to_message
+        if reply_to is not None:
+            reply_to.delete()
+        self.message.delete()
         if self.isLeech:
             if SOURCE_LINK is True:
                 try:
@@ -486,7 +490,7 @@ class MirrorLeechListener:
                         pass
             else:
                 pass
-            uploadmsg = sendMarkup(msg + pmwarn + logwarn + warnmsg, self.bot, self.message, InlineKeyboardMarkup(buttons.build_menu(2)))
+            uploadmsg = sendMarkup(msg + botpm, self.bot, self.message, InlineKeyboardMarkup(buttons.build_menu(2)))
             Thread(target=auto_delete_upload_message, args=(bot, self.message, uploadmsg)).start()
 
             if MIRROR_LOGS:	
